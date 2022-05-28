b8d4163974b10ab1bb7b1b8d3005c375a010c280
ArielRivas <102249863+ArielRivas@users.noreply.github.com>
2022-05-28 22:05:51 +0200

    adding score and time

--- molebuster.js
+++ molebuster.js
@@ -1,47 +1,51 @@
-const squares = document.querySelectorAll('.square');
-const mole = document.querySelector('.mole');
-const timeLeft = document.querySelector('#time-left');
-const score = document.querySelector('#score');
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
 
-let result = 0;
-let hitPosition;
-let currentTime = 6000;
-let timerId = null;
 
-const randomSquare = () => {
-  squares.forEach(square => {
-    square.classList.remove('mole');
-  });
+const randomTime = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
+};
 
-  let randomSquare = squares[Math.floor(Math.random() * 9)];
-  randomSquare.classList.add('mole');
+const randomHole = (holes) => {
+    const index  = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
 
-  hitPosition = randomSquare.id;
-}
-
-squares.forEach(square => {
-  square.addEventListener('click', () => {
-    if (square.id == hitPosition) {
-      result++;
-      score.textContent = result;
-      hitPosition = null;
+    if (hole === lastHole){
+        return randomHole(holes);
     }
-  })
-})
-
-const moveMole = () => {
-  timerId = setInterval(randomSquare, 600);
+    lastHole = hole;
+    return hole;
 };
 
-
-
-const tDown= () => {
- if (currentTime === 0) {
-   clearInterval(countDownTimerId);
-   clearInterval(timerId);
-   alert('GAME OVER! Your final score is ' + result);
- }
- setTimeout(currentTime, 1000 * 6);
+const getMole = () => {
+    const time = randomTime(500, 1000); 
+    const hole = randomHole(holes); 
+    hole.classList.add('up'); 
+    setTimeout(() => {
+        hole.classList.remove('up'); 
+        if(!timeUp) {
+            getMole();
+        }
+    }, time);
 };
 
-moveMole();
+const startGame = () => {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    getMole();
+    setInterval(() => timeUp = true, 30000); 
+};
+
+function hitMole (myScore){
+    if(!myScore.isTrusted) return; 
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+};
+
+moles.forEach(mole => mole.addEventListener('click', hitMole));
