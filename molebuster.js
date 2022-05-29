c9befec0ca6a2f658b9474a3f60f44130bbfcda3
ArielRivas <102249863+ArielRivas@users.noreply.github.com>
2022-05-29 22:18:15 +0200

    adding timer

--- molebuster.js
+++ molebuster.js
@@ -1,51 +1,69 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-let lastHole;
-let timeUp = false;
-let score = 0;
+const holesDiv = document.querySelector(".holes");
+const score = document.querySelector(".score");
+const time = document.querySelector(".time");
+const startButton = document.querySelector(".button");
+const display = document.querySelector(".game");
+const highScore = document.querySelector(".highScore");
+const gameOver = document.querySelector(".gameOver");
 
+let timeLeft;
+let scoreInit = 0;
+let maxScore = 0;
 
-const randomTime = (min, max) => {
-    return Math.round(Math.random() * (max - min) + min);
+for (let index = 1; index <= 16; index++) {
+  const hole = document.createElement("div");
+  hole.classList.add("hole");
+  holesDiv.appendChild(hole);
+
+  const dirt = document.createElement("img");
+  dirt.classList.add("dirt");
+  dirt.src = "../assets/dirt.png";
+  hole.appendChild(dirt);
+
+  const digglet = document.createElement("img");
+  digglet.classList.add("digglet");
+  digglet.src = "../assets/Diglett_EpEc.gif";
+  digglet.setAttribute("name", "digglet");
+  hole.appendChild(digglet);
 };
 
-const randomHole = (holes) => {
-    const index  = Math.floor(Math.random() * holes.length);
-    const hole = holes[index];
+window.addEventListener("click", (event)=>{
+  if (event.target.name === "digglet"){
+    //console.log(event.target)
+    setTimeout(()=>{
+      document.body.classList.toggle("flash")
+    }, 100)
+    document.body.classList.toggle("flash");
+    scoreInit = scoreInit + 1; //cambialo a 15 por ejemplo
+    score.textContent = scoreInit;
+  }
+});
 
-    if (hole === lastHole){
-        return randomHole(holes);
-    }
-    lastHole = hole;
-    return hole;
-};
 
-const getMole = () => {
-    const time = randomTime(500, 1000); 
-    const hole = randomHole(holes); 
-    hole.classList.add('up'); 
-    setTimeout(() => {
-        hole.classList.remove('up'); 
-        if(!timeUp) {
-            getMole();
-        }
-    }, time);
-};
-
-const startGame = () => {
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    getMole();
-    setInterval(() => timeUp = true, 30000); 
-};
-
-function hitMole (myScore){
-    if(!myScore.isTrusted) return; 
-    score++;
-    this.parentNode.classList.remove('up');
-    scoreBoard.textContent = score;
-};
-
-moles.forEach(mole => mole.addEventListener('click', hitMole));
+startButton.addEventListener("click", () => {
+  display.classList.add("close");
+  timeLeft = 10; //cambialo a 60 cuando acabes
+  scoreInit = 0;
+  score.textContent = scoreInit;
+  time.textContent = timeLeft;
+  let timer = setInterval(() => {
+    time.textContent = timeLeft;
+    if (timeLeft === 0){ //porqué me sale 0-1?????
+      clearInterval(timer);
+    }else{
+    timeLeft--;
+    time.textContent = timeLeft < 10 ? "0" + timeLeft : timeLeft;
+    const digglet = document.querySelectorAll(".digglet");
+    let movDigglet = Math.floor(Math.random()*digglet.length);//crear animaciones dentro de los eventos
+    digglet[movDigglet].style.pointerEvents = "all";
+    digglet[movDigglet].style.animation = "faceup 2s ease";
+    digglet[movDigglet].addEventListener("animationed", ()=>{
+      digglet[movDigglet].style.pointerEvents = "all";
+      digglet[movDigglet].style.animation = "facedown 0.5 ease";
+      digglet[movDigglet].addEventListener("animationed", ()=>{
+        digglet[movDigglet].style.pointerEvents = "none";
+      })
+    })
+  }
+  }, 900);
+});
